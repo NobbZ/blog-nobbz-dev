7d7d5274e58e9fd898023decf48ff0d92c250e63
Norbert Melzer <timmelzer@gmail.com>
2022-07-07 18:21:36 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "nobbz.dev - Website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    alejandra.url = "github:kamadorueda/alejandra?ref=1.5.0";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    devShells.x86_64-linux.default = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in pkgs.mkShell {
+      packages = builtins.attrValues {
+        inherit (pkgs) yarn rnix-lsp;
+        inherit (inputs.alejandra.packages.x86_64-linux) default;
+      };
+    };
+  };
+}
