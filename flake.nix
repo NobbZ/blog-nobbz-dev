7d4e0417b2e7b80a6a1cdc36bb6bf556102c1cf2
Norbert Melzer <timmelzer@gmail.com>
2022-07-09 15:59:36 +0200

    chore: Format code

--- flake.nix
+++ flake.nix
@@ -6,12 +6,19 @@
     alejandra.url = "github:kamadorueda/alejandra?ref=1.5.0";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    devShells.x86_64-linux.default = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in pkgs.mkShell {
-      packages = builtins.attrValues {
-        inherit (pkgs) yarn rnix-lsp;
-        inherit (inputs.alejandra.packages.x86_64-linux) default;
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    devShells.x86_64-linux.default = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+      pkgs.mkShell {
+        packages = builtins.attrValues {
+          inherit (pkgs) yarn rnix-lsp;
+          inherit (inputs.alejandra.packages.x86_64-linux) default;
+        };
       };
-    };
   };
 }
