8cbb00bed0d938760f177bccd22303c6927dc30c
Norbert Melzer <timmelzer@gmail.com>
2022-09-29 08:49:40 +0200

    refactor: flake.nix

--- flake.nix
+++ flake.nix
@@ -13,19 +13,20 @@
   } @ inputs: {
     formatter = inputs.nobbz.formatter;
 
-    packages.x86_64-linux.blog = let
+    packages.x86_64-linux = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       inherit (pkgs) callPackage;
-    in
-      callPackage ./.nix/blog.nix {inherit self;};
+    in {
+      blog = callPackage ./.nix/blog.nix {inherit self;};
+    };
 
     devShells.x86_64-linux.default = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
       pkgs.mkShell {
         packages = builtins.attrValues {
-          inherit (pkgs) yarn yarn2nix rnix-lsp;
-          inherit (inputs.nobbz.packages.x86_64-linux) alejandra;
+          inherit (pkgs) yarn yarn2nix;
+          inherit (inputs.nobbz.packages.x86_64-linux) alejandra nil;
         };
       };
   };
