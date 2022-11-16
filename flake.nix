7cec8040218f51c18850b85a23ed30cb4f03d26e
Norbert Melzer <timmelzer@gmail.com>
2022-11-16 07:15:23 +0100

    use dream2nix

--- flake.nix
+++ flake.nix
@@ -3,21 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    d2n.url = "github:nix-community/dream2nix";
     nobbz.url = "github:nobbz/nixos-config";
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
   outputs = {
     self,
+    d2n,
     nixpkgs,
+    nix-filter,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    inherit (nix-filter.lib) filter inDirectory matchExt;
+
+    self1 = d2n.lib.makeFlakeOutputs {
+      systems = ["x86_64-linux"];
+      config.projectRoot = ./.;
+      source = filter {
+        root = ./.;
+        include = [
+          (inDirectory "src")
+          (inDirectory "public")
+          (matchExt "js")
+          (matchExt "cjs")
+          (matchExt "mjs")
+          ./package.json
+          ./yarn.lock
+        ];
+      };
+      packageOverrides.blog-nobbz-dev.copyBlog = {
+        installPhase = ''
+          mkdir -p $out
+          cp -rv ./dist/* $out
+        '';
+      };
+    };
+  in {
     formatter = inputs.nobbz.formatter;
 
     packages.x86_64-linux = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       inherit (pkgs) callPackage;
     in {
-      blog = callPackage ./.nix/blog.nix {inherit self;};
+      blog = self1.packages.x86_64-linux.blog-nobbz-dev;
     };
 
     devShells.x86_64-linux.default = let
