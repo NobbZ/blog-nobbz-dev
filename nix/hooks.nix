3aeb9bcf0d7c4214ee7c15254a4384583abda052
Norbert Melzer <timmelzer@gmail.com>
2022-12-20 22:47:11 +0100

    chore: enable hooks for all relevant astro files

--- nix/hooks.nix
+++ nix/hooks.nix
@@ -3,4 +3,5 @@
   perSystem.pre-commit.settings.hooks.nixpkgs-fmt.enable = false;
   perSystem.pre-commit.settings.hooks.alejandra.enable = true;
   perSystem.pre-commit.settings.hooks.eslint.enable = true;
+  perSystem.pre-commit.settings.settings.eslint.extensions = "\\.(js|ts|jsx|tsx|astro|md|mdx|cjs|ts)$";
 }
