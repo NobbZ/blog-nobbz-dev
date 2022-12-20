d73c75d57823f8a0aa2b8e1a5fa4a60b70714d9d
Norbert Melzer <timmelzer@gmail.com>
2022-12-20 22:52:14 +0100

    chore: enable prettier in pre-commit

--- nix/hooks.nix
+++ nix/hooks.nix
@@ -4,4 +4,5 @@
   perSystem.pre-commit.settings.hooks.alejandra.enable = true;
   perSystem.pre-commit.settings.hooks.eslint.enable = true;
   perSystem.pre-commit.settings.settings.eslint.extensions = "\\.(js|ts|jsx|tsx|astro|md|mdx|cjs|ts)$";
+  perSystem.pre-commit.settings.hooks.prettier.enable = true;
 }
