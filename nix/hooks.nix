a637a31eaf6731b5977125f3640358201d0f6bdc
Norbert Melzer <timmelzer@gmail.com>
2023-02-27 19:56:25 +0100

    ci: fix eslint to properly work within this repo

--- nix/hooks.nix
+++ nix/hooks.nix
@@ -1,8 +1,13 @@
-{...}: {
-  perSystem.pre-commit.check.enable = true;
-  perSystem.pre-commit.settings.hooks.nixpkgs-fmt.enable = false;
-  perSystem.pre-commit.settings.hooks.alejandra.enable = true;
-  perSystem.pre-commit.settings.hooks.eslint.enable = true;
-  perSystem.pre-commit.settings.settings.eslint.extensions = "\\.(js|ts|jsx|tsx|astro|md|mdx|cjs|ts)$";
-  perSystem.pre-commit.settings.hooks.prettier.enable = true;
+{self, ...}: {
+  perSystem = {pkgs, ...}: {
+    pre-commit.check.enable = true;
+    pre-commit.settings.hooks.nixpkgs-fmt.enable = false;
+    pre-commit.settings.hooks.alejandra.enable = true;
+    pre-commit.settings.hooks.prettier.enable = true;
+
+    pre-commit.settings.hooks.eslint.enable = true;
+    pre-commit.settings.hooks.eslint.pass_filenames = false;
+    pre-commit.settings.settings.eslint.binPath = "${pkgs.nodePackages.eslint}/bin/eslint src";
+    pre-commit.settings.settings.eslint.extensions = "\\.(js|ts|jsx|tsx|astro|md|mdx|cjs|ts)$";
+  };
 }
