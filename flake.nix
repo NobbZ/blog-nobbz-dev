ebcb2ee87c2ca991efe2ac151290a9eec6512ac7
Norbert Melzer <timmelzer@gmail.com>
2024-01-05 22:51:40 +0100

    tabula rasa

--- flake.nix
+++ flake.nix
@@ -1,58 +1,44 @@
 {
   description = "nobbz.dev - Website";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
-    d2n.url = "github:nix-community/dream2nix";
-    d2n.inputs.all-cabal-json.follows = "nixpkgs";
-    nobbz.url = "github:nobbz/nixos-config";
-    nix-filter.url = "github:numtide/nix-filter";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    pre-commit.url = "github:cachix/pre-commit-hooks.nix";
-  };
-
   outputs = {
     self,
-    d2n,
     nixpkgs,
     nix-filter,
     flake-parts,
-    pre-commit,
     ...
-  } @ inputs: let
-    systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-  in
+  } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      inherit systems;
+      systems = import inputs.systems;
 
-      imports = [d2n.flakeModuleBeta pre-commit.flakeModule ./nix/blog.nix ./nix/hooks.nix];
+      imports = [];
 
       perSystem = {
-        config,
         pkgs,
-        self',
         inputs',
-        system,
         ...
-      }: {
-        formatter = inputs'.nobbz.formatter;
+      }: let
+        mkMiniShell = import inputs.mkshell-minimal pkgs;
+      in {
+        formatter = pkgs.alejandra;
 
-        apps.serve.program = "${pkgs.writeShellScript "serve" ''
-          set -e
-          result=$(nom build .#blog --print-out-paths)
-          ${pkgs.miniserve}/bin/miniserve -p 3001 --index index.html ''${result}
-        ''}";
-
-        packages.default = self'.packages.blog;
-
-        devShells.default = pkgs.mkShell {
+        devShells.default = mkMiniShell {
           packages = builtins.attrValues {
-            inherit (pkgs) yarn nodejs_20 yarn2nix;
-            inherit (pkgs.nodePackages) gatsby-cli;
-            inherit (inputs.nobbz.packages.${system}) alejandra nil;
+            inherit (pkgs) zola alejandra nil;
           };
-          shellHook = config.pre-commit.installationScript;
         };
       };
     };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    nix-filter.url = "github:numtide/nix-filter";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    mkshell-minimal.url = "github:viperML/mkshell-minimal";
+    mkshell-minimal.flake = false;
+
+    systems.url = "github:nix-systems/default-linux";
+    systems.flake = false;
+  };
 }
