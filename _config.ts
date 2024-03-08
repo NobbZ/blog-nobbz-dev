8d98b6e036effc6cfb0cb3c84132cd0a24d38fdc
Norbert Melzer <timmelzer@gmail.com>
2024-03-08 10:37:36 +0100

    starting with redesign

--- _config.ts
+++ _config.ts
@@ -1,14 +1,29 @@
 import lume from "lume/mod.ts";
+
+/* Import plugins */
 import attributes from "lume/plugins/attributes.ts";
 import base_path from "lume/plugins/base_path.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
 import feed from "lume/plugins/feed.ts";
+import metas from "lume/plugins/metas.ts";
+import postcss from "lume/plugins/postcss.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
 
-const site = lume();
+/* Set basic configuration */
+const site = lume({
+  src: "src",
+  server: { port: 10123 },
+});
 
+/* Load and configure plugins */
 site.use(attributes());
 site.use(base_path());
 site.use(code_highlight());
 site.use(feed());
+site.use(metas());
+site.use(tailwindcss());
+
+// needs to be added *after* tailwindcss
+site.use(postcss());
 
 export default site;
---
+++ src/index.md
@@ -0,0 +1,26 @@
+---
+layout: layouts/page.vto
+---
+
+# Welcome to my website
+
+This is my first page using **Lume**, a *static* site generator for Deno.
+
+We hope you enjoy it.
+
+asdd
+asdasd
+
+asdasd
+
+asds
+
+
+dasddd
+
+# heading 1
+## heading 2
+### heading 3
+#### heading 4
+##### heading 5
+###### heading 6
