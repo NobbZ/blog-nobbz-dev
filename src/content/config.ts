9eba117b1b91aa27ce61b13149c05a00345fecac
Norbert Melzer <timmelzer@gmail.com>
2024-06-16 22:16:14 +0200

    tweak author display in post listing

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,25 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z, reference } from "astro:content";
+
+const authors = defineCollection({
+  type: "content",
+  schema: z.object({
+    first_name: z.string(),
+    last_name: z.string(),
+    nick_name: z.string(),
+    social: z.object({
+      amazon: z.string(),
+      github: z.string(),
+      gitlab: z.string(),
+      "ko-fi": z.string(),
+      linkedin: z.string(),
+      twitter: z.string(),
+      web: z.object({
+        url: z.string(),
+        name: z.string(),
+      }),
+    }),
+  }),
+});
 
 const blog = defineCollection({
   type: "content",
@@ -6,11 +27,12 @@ const blog = defineCollection({
     title: z.string(),
     tags: z.array(z.string()),
     date: z.date(),
-    author: z.string(),
     description: z.string(),
+    author: reference("authors"),
   }),
 });
 
 export const collections = {
+  authors,
   blog,
 };
