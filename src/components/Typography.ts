ad38865bfcafe9aa19a568d7ff7ed4c0489633e8
Norbert Melzer <timmelzer@gmail.com>
2024-06-16 12:02:18 +0200

    style pre-component a bit

--- src/components/Typography.ts
+++ src/components/Typography.ts
@@ -1,3 +1,4 @@
 import H1 from "./typography/H1.astro";
+import Pre from "./typography/Pre.astro";
 
-export { H1 };
+export { H1, Pre };
---
+++ src/scripts/components.ts
@@ -0,0 +1,5 @@
+import { Pre } from "../components/Typography";
+
+export const components = {
+  pre: Pre,
+};
