24a105160b30359a418e413e6a95ac2804ad33a2
Norbert Melzer <timmelzer@gmail.com>
2024-06-16 23:42:33 +0200

    fix formatting

--- src/scripts/paths.ts
+++ src/scripts/paths.ts
@@ -1,16 +1,16 @@
 import type { CollectionEntry } from "astro:content";
 
-import { DateTime } from 'luxon';
+import { DateTime } from "luxon";
 
 export const postPath = (post: CollectionEntry<"blog">): string => {
   const date = DateTime.fromJSDate(post.data.date).toFormat("yyyy-MM-dd");
   const slug = post.slug;
 
   return `/blog/${date}-${slug}`;
-}
+};
 
 export const authorPath = (author: CollectionEntry<"authors">): string => {
   const slug = author.slug;
 
   return `/author/${slug}`;
-}
+};
