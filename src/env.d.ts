d2e108eca81a00ab792dbe5364b56698ba64f116
Norbert Melzer <timmelzer@gmail.com>
2024-07-11 20:55:56 +0200

    add more menu

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-import "../.astro/types";
 
 declare module "*.astro" {
   import type { AstroComponentFactory } from "astro/runtime/server";
