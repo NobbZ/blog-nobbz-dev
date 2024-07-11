10b92eb4df46e5825e4401ee87560b98f2576e99
Norbert Melzer <timmelzer@gmail.com>
2024-07-11 21:01:12 +0200

    remove unneeded code

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,3 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-declare module "*.astro" {
-  import type { AstroComponentFactory } from "astro/runtime/server";
-  const component: AstroComponentFactory;
-  export default component;
-}
