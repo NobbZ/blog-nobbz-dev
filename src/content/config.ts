88f9dbaca14851d6b8934702fb9b35a50a55848e
Norbert Melzer <timmelzer@gmail.com>
2024-07-23 16:37:54 +0200

    rework author schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,34 @@
 import { defineCollection, z, reference } from "astro:content";
 
+const fallback = z.object({
+  icon: z.string().url(),
+  displayText: z.string(),
+  href: z.string().url(),
+}).partial({ icon: true });
+
 const authors = defineCollection({
   type: "content",
   schema: z.object({
     first_name: z.string(),
     last_name: z.string(),
     nick_name: z.string(),
+    funding: z.object({
+      amazon: z.string().url(),
+      github: z.string(),
+      kofi: z.string(),
+      patreon: z.string(),
+      polar: z.string(),
+    }).catchall(fallback).partial(),
     social: z.object({
-      amazon: z.string(),
       github: z.string(),
       gitlab: z.string(),
-      "ko-fi": z.string(),
       linkedin: z.string(),
       twitter: z.string(),
-      web: z.object({
-        url: z.string(),
-        name: z.string(),
-      }),
-    }),
-  }),
+    }).catchall(fallback).partial(),
+    web: z.object({
+      homepage: z.string().url(),
+    }).catchall(fallback).partial(),
+  }).partial().required({ nick_name: true }),
 });
 
 const blog = defineCollection({
