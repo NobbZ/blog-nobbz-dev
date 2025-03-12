b1d65d3a1cb60296d8e3a70ac6ba06e8cbf65695
Norbert Melzer <timmelzer@gmail.com>
2025-03-12 20:19:48 +0100

    fix name in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
 import eslint from "@eslint/js";
-import tseslinst from "typescript-eslint";
+import tseslint from "typescript-eslint";
 import astro from "eslint-plugin-astro";
 
 export default [
   eslint.configs.recommended,
-  ...tseslinst.configs.recommended,
+  ...tseslint.configs.recommended,
   ...astro.configs.recommended,
   { ignores: ["dist/*", ".astro/*"] },
   { languageOptions: { globals: { astroHTML: "readonly" } } },
