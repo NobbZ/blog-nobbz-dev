6ce909056933e59634e4d6b538a8316e558f35f7
Norbert Melzer <timmelzer@gmail.com>
2025-03-29 15:53:16 +0100

    docs for sorter

--- src/scripts/sorter.ts
+++ src/scripts/sorter.ts
@@ -9,5 +9,14 @@ export type DateSortable = {
   };
 };
 
+/**
+ * Can be used for any `ContentCollection` that has a `data.date` field to sort
+ * by it.
+ *
+ * @param a first item to compare
+ * @param b second item to compare
+ * @returns positive number if `a` is older than `b`, negative number if `a` is
+ *   newer than `b`
+ */
 export const byDate = (a: DateSortable, b: DateSortable): number =>
   b.data.date.getTime() - a.data.date.getTime();
