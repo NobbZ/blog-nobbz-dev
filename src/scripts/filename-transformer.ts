9023d1b31eac2f49646b250fe24f7287175330e7
Norbert Melzer <timmelzer@gmail.com>
2025-03-31 10:18:17 +0200

    classlist refactor

--- src/scripts/filename-transformer.ts
+++ src/scripts/filename-transformer.ts
@@ -19,8 +19,27 @@ export function filenameTransformer(): ShikiTransformer {
       if (filename) {
         code.properties.dataFilename = filename;
 
-        code.properties.class +=
-          " relative !pt-[2rem] before:content-[attr(data-filename)] before:absolute before:top-0 before:left-0 before:w-full before:bg-crust before:border-t-2 before:border-l-2 before:border-r-2 before:border-base before:border-solid before:font-bold before:px-4 before:py-1 before:whitespace-nowrap before:overflow-x-auto";
+        code.properties.class += " ";
+        code.properties.class += [
+          "relative",
+          "!pt-[2rem]",
+          "before:content-[attr(data-filename)]",
+          "before:absolute",
+          "before:top-0",
+          "before:left-0",
+          "before:w-full",
+          "before:bg-crust",
+          "before:border-t-2",
+          "before:border-l-2",
+          "before:border-r-2",
+          "before:border-base",
+          "before:border-solid",
+          "before:font-bold",
+          "before:px-4",
+          "before:py-1",
+          "before:whitespace-nowrap",
+          "before:overflow-x-auto",
+        ].join(" ");
       }
     },
   };
